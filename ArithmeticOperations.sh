4a31d024756cf904bb4fb3ed535d4d267e1ab706
ShitalSonwane <nilamkurhade87@gmail.com>
2020-02-29 12:02:59 +0530

    Arithmetic Operations

---
+++ ArithmeticOperations.sh
@@ -0,0 +1,14 @@
+#!bin/bash -x
+
+read -p "Enter first value: " a
+read -p "Enter second value: " b
+read -p "Enter third value: " c
+
+Operation1=$(($a + $b * $c))
+Operation2=$(($a % $b + $c)) 
+Operation3=$(($c + $a / $b))
+Operation4=$(($a * $b + $c))
+echo $Operation1
+echo $Operation2
+echo $Operation3
+echo $Operation4
