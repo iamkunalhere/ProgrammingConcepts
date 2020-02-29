c4792f1babc778480409b14b86b05c11f7a45ca6
ShitalSonwane <nilamkurhade87@gmail.com>
2020-02-29 12:57:26 +0530

    Updated

--- ArithmeticOperations.sh
+++ ArithmeticOperations.sh
@@ -6,7 +6,7 @@ Operation1=$(($a + $b * $c))
 Operation2=$(($a % $b + $c)) 
 Operation3=$(($c + $a / $b))
 Operation4=$(($a * $b + $c))
-echo $Operation1
-echo $Operation2
-echo $Operation3
-echo $Operation4
+echo Result of first Operation$Operation1
+echo Result of second Operation$Operation2
+echo Result of third Operation$Operation3
+echo Result of fourth Operation$Operation4
